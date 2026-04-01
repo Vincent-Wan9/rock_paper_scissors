6e822f2e1f5439e2cc9d7378185e0cd5c8163f0c
Vincent-Wan9 <vincentwang9672@gmail.com>
2026-03-31 20:55:34 -0700

    add button feature for human selection

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 function getComputerChoice(){
-    if(Math.random()<=1/3){
+    const rand = Math.random();
+    if(rand<=1/3){
         return "rock";
-    } else if (Math.random()>(1/3) && Math.random()<=(2/3)){
+    } else if (rand>(1/3) && rand<=(2/3)){
         return "paper";
     } else {
         return "scissors";
@@ -15,48 +16,74 @@ function getHumanChoice(){
 }
 
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice, output){
     humanChoice = humanChoice.toLowerCase();
+    if (!["rock", "paper", "scissors"].includes(humanChoice)) {
+        output.textContent = "Invalid input!";
+        return;
+    }
 
     if(humanChoice==="rock" && computerChoice==="paper"){
         computerScore++;
-        console.log("You lose! rock cannot beats paper");
+        output.textContent = "You lose! rock cannot beat paper";
     } else if (humanChoice==="rock" && computerChoice==="scissors"){
         humanScore++;
-        console.log("You win! Rock beats scissors");
+        output.textContent = "You win! Rock beats scissors";
     } else if (humanChoice==="paper" && computerChoice==="scissors"){
         computerScore++;
-        console.log("You lose! paper cannot beats scissors");
+        output.textContent = "You lose! paper cannot beat scissors";
     } else if (humanChoice==="paper" && computerChoice==="rock"){
         humanScore++;
-        console.log("You win! Paper beats Rock");
+        output.textContent = "You win! Paper beats Rock";
     } else if (humanChoice==="scissors" && computerChoice==="rock"){
         computerScore++;
-        console.log("You lose! scissors cannot beats rock");
+        output.textContent = "You lose! scissors cannot beat rock";
     } else if (humanChoice==="scissors" && computerChoice==="paper"){
         humanScore++;
-        console.log("You win! Scissors beats paper");
+        output.textContent = "You win! Scissors beat paper";
     } else {
-        console.log("No winner today!");
+        output.textContent = "No winner today!";
     }
 }
+
+
+
+/*
+function playGame(event){
+    
+    event.target.value
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+
+
+
+    console.log(`final score for human is ${humanScore}, and final score for computer is ${computerScore}.
+        Thus, the winner is ${(humanScore>=computerScore)? "human": "computer"}!`);
+}
+*/
 
 let humanScore = 0;
 let computerScore = 0;
+let buttons = document.querySelectorAll("button");
+let output = document.querySelector(".output");
+let summary = document.querySelector(".summary");
 
-function playGame(){
-    
-
-    
-    for (let i = 1; i <= 5; i++){
-        const humanSelection = getHumanChoice();
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const humanSelection = button.value;
         const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+        playRound(humanSelection, computerSelection, output);
 
-    console.log(`final score for human is ${humanScore}, and final score for computer is ${computerScore}.
-        Thus, the winner is ${(humanScore>computerScore)? "human": "computer"}!`);
-}
+        if(humanScore>=5 || computerScore >= 5){
+            summary.textContent = `final score for human is ${humanScore}, and final score for computer is ${computerScore}.
+            Thus, the winner is ${(humanScore>=computerScore)? "human": "computer"}!`;
+
+            buttons.forEach(btn => btn.disabled = true);
+        }
+    });
+});
 
 
-playGame()
+
